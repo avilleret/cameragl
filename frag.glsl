9c59931baa486d0fc6b5609b25737d3fe67b5d87
Antoine Villeret <pi@longuevue.(none)>
2014-05-23 14:01:40 +0000

    add per-color gain and offset

--- frag.glsl
+++ frag.glsl
@@ -4,10 +4,14 @@ uniform sampler2D Texture;
 uniform float invert; // unfortunately, Raspberry Pi driver doesn't seems to support uniform initializer...
 uniform float alpha;
 uniform float gain;
+uniform float offset_r,offset_g,offset_b;
+uniform float gain_r,gain_g,gain_b;
 void main() 
 { 
   vec4 color = texture2D(Texture, TexCoordOut);
   color.rgb = color.rgb * (1. - invert) + invert * (vec3(1.,1.,1.) - color.rgb);
+  color.rgb += vec3(offset_r,offset_g,offset_b);
+  color.rgb *= vec3(gain_r, gain_g, gain_b);
   color.rgb*=gain;
   color.a=alpha;
   gl_FragColor = color; 
