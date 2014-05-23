0b67b6c7a9775362dfad1300857b733a7e1c548c
Antoine Villeret <pi@longuevue.(none)>
2014-05-23 14:04:13 +0000

    python script to read sensor attached to GPIO and trig OSC sequence

---
+++ example/read_sensor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import OSC
+import time
+GPIO.setmode(GPIO.BCM)
+
+buttonPin = 9
+
+GPIO.setup(buttonPin,GPIO.IN)
+GPIO.setup(buttonPin,GPIO.IN)
+
+# setup OSC
+client = OSC.OSCClient()
+client.connect( ('127.0.0.1', 9002) )
+msgPlay = OSC.OSCMessage()
+msgPlay.setAddress("/play")
+msgPlay.append(1)
+msgStop = OSC.OSCMessage()
+msgStop.setAddress("/play")
+msgStop.append(1)
+
+prev_input = 0
+print("start polling")
+while True:
+  #take a reading
+  input = GPIO.input(buttonPin)
+  #if the last reading was low and this one high, print
+  if (prev_input != input):
+    print(input)
+    #update previous input
+    prev_input = input
+    if (input==0):
+      try:
+        client.send(msgPlay)
+      except:
+        print("can't connect to OSC")
+      else:
+        print("play")
+      time.sleep(0.5) # 1s debounce
+    else:
+      time.sleep(0.5) # 5s debounce
