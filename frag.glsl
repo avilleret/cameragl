029e1c5a54bbb20a15495324e275737f0dd16488
Antoine Villeret <antoine.villeret@gmail.com>
2014-06-10 20:23:08 +0200

    update fragment shader
    apply gamma before gray conversion
    only 3 three parameters :
    invert : fade to inverted black and white
    white : fade to white
    gamma : gamma applied to black and white before conversion

--- frag.glsl
+++ frag.glsl
@@ -1,43 +1,25 @@
-precision mediump float; 
+precision mediump float; // nécessaire pour la Pi ?
 varying vec2 TexCoordOut;
 uniform sampler2D Texture;
 uniform float invert; // unfortunately, Raspberry Pi driver doesn't seems to support uniform initializer...
-uniform float alpha;
-uniform float gain;
-uniform float offset_r,offset_g,offset_b;
-uniform float gain_r,gain_g,gain_b;
-uniform float hue, saturation;
-
-vec3 rgb2hsv(vec3 c)
-{
-    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-
-    float d = q.x - min(q.w, q.y);
-    float e = 1.0e-10;
-    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-}
-
-vec3 hsv2rgb(vec3 c)
-{
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
+uniform float white;
+uniform float gamma;
 
 void main() 
 { 
-  vec4 color = texture2D(Texture, TexCoordOut);
-  color.rgb = color.rgb * (1. - invert) + invert * (vec3(1.,1.,1.) - color.rgb);
-  color.rgb += vec3(offset_r,offset_g,offset_b);
-  color.rgb *= vec3(gain_r, gain_g, gain_b);
-  color.rgb*=gain;
+  vec4 color = texture2D(Texture, TexCoordOut); //  version RPi
+  //~vec4 color = texture2D(Texture, (gl_TextureMatrix[0] * gl_TexCoord[0]).st); // version Gem
+  color.a  = 1.;
   
-  vec3 hsv = rgb2hsv(color.rgb);
-  hsv.xy*=vec2(hue,saturation);
-  color.rgb=hsv2rgb(hsv);
-  
-  color.a=alpha;
+  // expansion de gamma pour la conversion en niveau de gris
+  vec3 color_gamma = pow(color.rgb,vec3( gamma ));
+
+  // conversion en niveau de gris inversé
+  float ibw = 1. - (0.299 * color_gamma.r + 0.587 * color_gamma.g + 0.114 * color_gamma.b); // conversion en 
+
+  color.rgb = mix(color.rgb, vec3(ibw,ibw,ibw), invert);
+  color.rgb = mix(color.rgb, vec3(1.,1.,1.), white);
+
   gl_FragColor = color; 
 }
+
